e517c3dca3521f41dc6106d365d9f46f7fbb8e02
Robin Vigier / B!nro <rob.vigier@gmail.com>
2026-06-02 11:21:41 +0200

    🚀 setup three

---
+++ src/Scene/Scene.js
@@ -0,0 +1,78 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import Stats from "three/examples/jsm/libs/stats.module.js";
+
+class Scene {
+	constructor() {
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
+		this.setupScene();
+		this.setupCamera();
+		this.setupRenderer();
+		this.addCube();
+		this.setupControls();
+		window.addEventListener("resize", this.onResize);
+	}
+
+	init() {
+		this.setupStats();
+		this.renderer.setAnimationLoop(this.tick);
+	}
+
+	setupScene() {
+		this.scene = new THREE.Scene();
+	}
+
+	setupCamera() {
+		this.camera = new THREE.PerspectiveCamera(
+			75,
+			this.width / this.height,
+			0.1,
+			1000
+		);
+
+		this.camera.position.z = 5;
+	}
+
+	setupControls() {
+		this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+	}
+
+	setupRenderer() {
+		this.renderer = new THREE.WebGLRenderer();
+		this.renderer.setSize(this.width, this.height);
+		document.body.appendChild(this.renderer.domElement);
+	}
+
+	addCube() {
+		const geometry = new THREE.BoxGeometry(1, 1, 1);
+		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+		const cube = new THREE.Mesh(geometry, material);
+		this.scene.add(cube);
+	}
+
+	setupStats() {
+		this.stats = new Stats();
+		document.body.appendChild(this.stats.dom);
+	}
+
+	render() {
+		this.renderer.render(this.scene, this.camera);
+	}
+
+	tick = (time) => {
+		this.stats.begin();
+		this.render();
+		this.stats.end();
+	};
+
+	onResize = () => {
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
+		this.renderer.setSize(this.width, this.height);
+		this.camera.aspect = this.width / this.height;
+		this.camera.updateProjectionMatrix();
+	};
+}
+
+export default Scene;
